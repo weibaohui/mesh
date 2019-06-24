9c83a10d23f6d7159577c03d71f38e8bd7f70206
weibaohui <weibaohui@yeah.net>
2019-06-24 23:07:54 +0800

    tty

--- pkg/webapi/ui/tty.go
+++ pkg/webapi/ui/tty.go
@@ -1,12 +1,11 @@
 package ui
 
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/emicklei/go-restful"
+	"github.com/gorilla/websocket"
 	"github.com/weibaohui/mesh/pkg/server"
-	"io"
-	"io/ioutil"
 	v1 "k8s.io/api/core/v1"
 	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
@@ -15,6 +14,42 @@ import (
 	"log"
 	"net/http"
 )
+
+type terminal struct {
+	conn          *websocket.Conn
+	size          chan *remotecommand.TerminalSize
+	ns            string
+	podName       string
+	containerName string
+}
+
+func (t *terminal) Read(p []byte) (n int, err error) {
+	_, ps, err := t.conn.ReadMessage()
+	if err != nil {
+		return 0, err
+	}
+	size := remotecommand.TerminalSize{}
+	if err = json.Unmarshal(ps, &size); err == nil {
+		t.size <- &size
+		return 0, nil
+	} else {
+		fmt.Println(err.Error())
+		return copy(p, ps), nil
+	}
+}
+func (t *terminal) Write(p []byte) (n int, err error) {
+	writer, err := t.conn.NextWriter(websocket.TextMessage)
+	if err != nil {
+		return 0, nil
+	}
+	defer writer.Close()
+	return writer.Write(p)
+}
+func (t *terminal) Next() *remotecommand.TerminalSize {
+	size := <-t.size
+	fmt.Println("size", size)
+	return size
+}
 
 // 	ws.Route(ws.GET("/ns/{ns}/podName/{podName}/exec")
 func PodExec(request *restful.Request, response *restful.Response) {
@@ -34,84 +69,28 @@ func PodExec(request *restful.Request, response *restful.Response) {
 	}
 	defer c.Close()
 
-	logEvent := make(chan []byte)
-	exec := executor(ns, podName, containerName)
-
-	go func() {
-		for {
-			if _, reader, err := c.NextReader(); err == nil {
-				var execOut bytes.Buffer
-				go func() {
-					err = exec.Stream(remotecommand.StreamOptions{
-						Stdin:             reader,
-						Stdout:            &execOut,
-						Stderr:            nil,
-						Tty:               true,
-						TerminalSizeQueue: nil,
-					})
-				}()
-
-				fmt.Println("读取一行")
-				line, err := ioutil.ReadAll(&execOut)
-				fmt.Println("读取", string(line))
-				if err != nil {
-					fmt.Println("读取", err.Error())
-				}
-				logEvent <- line
-
-			}
-		}
-	}()
-
-	for {
-		select {
-		case item, ok := <-logEvent:
-			fmt.Println("logEvent接受一行")
-			if !ok {
-
-				break
-			}
-			if err := writeData(c, item); err != nil {
-				fmt.Println("writeData", err.Error())
-			}
-
-		}
+	t := &terminal{
+		conn:          c,
+		ns:            ns,
+		podName:       podName,
+		containerName: containerName,
 	}
 
-	// for {
-	// 	_, command, err := c.ReadMessage()
-	// 	if err != nil {
-	// 		log.Println("read:", err)
-	// 		break
-	// 	}
-	// 	s := strings.TrimSpace(string(command))
-	// 	fmt.Println("收到", s, strings.HasSuffix(s, "]"))
-	// 	commands := strings.SplitN(s, " ", -1)
-	// 	fmt.Println("split result", commands, len(commands))
-	// 	for k, v := range commands {
-	// 		fmt.Println("split ff ", k, v)
-	// 	}
-	// 	result, err := execIntoPod(ns, podName, containerName, commands)
-	// 	if err != nil {
-	// 		fmt.Println("execIntoPod", err.Error())
-	// 	}
-	// 	err = writeData(c, []byte(result))
-	// 	if err != nil {
-	// 		fmt.Println("write:", err)
-	// 		break
-	// 	}
-	// }
+	err = executor(t)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
 }
 
-func executor(ns, podName, containerName string) remotecommand.Executor {
-
+func executor(t *terminal) error {
+	var defaultCommand = []string{"/bin/sh", "-c", `TERM=xterm-256color; export TERM; [ -x /bin/bash ] && ([ -x /usr/bin/script ] && /usr/bin/script -q -c "/bin/bash" /dev/null || exec /bin/bash) || exec /bin/sh`};
 	mctx := server.GlobalContext()
 	config := server.GlobalKubeConfig()
 	req := mctx.K8s.CoreV1().RESTClient().Post().
 		Resource("pods").
-		Name(podName).
-		Namespace(ns).
+		Name(t.podName).
+		Namespace(t.ns).
 		SubResource("exec").
 		VersionedParams(
 			&v1.PodExecOptions{
@@ -119,138 +98,28 @@ func executor(ns, podName, containerName string) remotecommand.Executor {
 				Stdin:     true,
 				Stdout:    true,
 				TTY:       true,
-				Container: containerName,
-				Command:   []string{"/bin/sh"},
+				Container: t.containerName,
+				Command:   defaultCommand,
 			},
 			scheme.ParameterCodec,
 		)
 	restConfig, err := clientcmd.BuildConfigFromFlags("", config)
 	if err != nil {
 		fmt.Errorf("clientcmd.BuildConfigFromFlags =%s ", err.Error())
-		return nil
+		return err
 	}
 	exec, err := remotecommand.NewSPDYExecutor(restConfig, http.MethodPost, req.URL())
 	if err != nil {
 		fmt.Errorf("remotecommand.NewSPDYExecutor =%s ", err.Error())
-		return nil
+		return err
 	}
-	return exec
-
-}
-
-func execIntoPodReader(in io.Reader, logEvent chan []byte, ns, podName, containerName string) {
-
-	var execOut bytes.Buffer
-	mctx := server.GlobalContext()
-	config := server.GlobalKubeConfig()
-	req := mctx.K8s.CoreV1().RESTClient().Post().
-		Resource("pods").
-		Name(podName).
-		Namespace(ns).
-		SubResource("exec").
-		VersionedParams(
-			&v1.PodExecOptions{
-				TypeMeta:  v12.TypeMeta{},
-				Stdin:     true,
-				Stdout:    true,
-				TTY:       true,
-				Container: containerName,
-				Command:   []string{"/bin/sh"},
-			},
-			scheme.ParameterCodec,
-		)
-	restConfig, err := clientcmd.BuildConfigFromFlags("", config)
-	if err != nil {
-		fmt.Errorf("clientcmd.BuildConfigFromFlags =%s ", err.Error())
-		return
-	}
-	exec, err := remotecommand.NewSPDYExecutor(restConfig, http.MethodPost, req.URL())
-	if err != nil {
-		fmt.Errorf("remotecommand.NewSPDYExecutor =%s ", err.Error())
-		return
-	}
-
 	err = exec.Stream(remotecommand.StreamOptions{
-		Stdin:  in,
-		Stdout: &execOut,
-		Tty:    true,
+		Stdin:             t,
+		Stdout:            t,
+		Stderr:            t,
+		Tty:               true,
+		TerminalSizeQueue: t,
 	})
-
-	if err != nil {
-		fmt.Errorf("could not execute: %v", err)
-		return
-	}
-
-	for {
-		line, err := execOut.ReadBytes('\n')
-		if err == io.EOF {
-			break
-		}
-		logEvent <- line
-
-	}
-
-}
-
-func execIntoPod(ns, podName, containerName string, commands []string) (string, error) {
-
-	var execOut bytes.Buffer
-	var execErr bytes.Buffer
-	mctx := server.GlobalContext()
-	config := server.GlobalKubeConfig()
-	req := mctx.K8s.CoreV1().RESTClient().Post().
-		Resource("pods").
-		Name(podName).
-		Namespace(ns).
-		SubResource("exec").
-		VersionedParams(
-			&v1.PodExecOptions{
-				TypeMeta:  v12.TypeMeta{},
-				Stdin:     false,
-				Stdout:    true,
-				Stderr:    true,
-				TTY:       true,
-				Container: containerName,
-				Command:   commands,
-			},
-			scheme.ParameterCodec,
-		)
-	restConfig, err := clientcmd.BuildConfigFromFlags("", config)
-	if err != nil {
-		fmt.Errorf("clientcmd.BuildConfigFromFlags =%s ", err.Error())
-		return "", err
-	}
-	exec, err := remotecommand.NewSPDYExecutor(restConfig, http.MethodPost, req.URL())
-	if err != nil {
-		fmt.Errorf("remotecommand.NewSPDYExecutor =%s ", err.Error())
-		return "", err
-	}
-
-	err = exec.Stream(remotecommand.StreamOptions{
-		// Stdin:             strings.NewReader(commands),
-		Stdout: &execOut,
-		Stderr: &execErr,
-		Tty:    true,
-	})
-
-	if err != nil {
-		fmt.Errorf("could not execute: %v", err)
-		return "", err
-	}
-
-	if execErr.Len() > 0 {
-		fmt.Errorf("stderr: %v", execErr.String())
-		return "", err
-	}
-	for {
-		line, err := execOut.ReadBytes('\n')
-		if err == io.EOF {
-			break
-		}
-		fmt.Println(string(line))
-
-	}
-
-	return execOut.String(), nil
+	return err
 
 }

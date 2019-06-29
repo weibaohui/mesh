da22bdfd12be6358940e6c01fdc5969dcf4a7865
weibaohui <weibaohui@yeah.net>
2019-06-30 00:06:36 +0800

    容器注入功能

--- pkg/webapi/ui/deploy.go
+++ pkg/webapi/ui/deploy.go
@@ -2,17 +2,20 @@ package ui
 
 import (
 	"github.com/emicklei/go-restful"
+	"github.com/sirupsen/logrus"
 	"github.com/weibaohui/mesh/pkg/constants"
 	"github.com/weibaohui/mesh/pkg/server"
 	"github.com/weibaohui/mesh/pkg/utils"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func Inject(request *restful.Request, response *restful.Response) {
 	ns := request.PathParameter("ns")
 	name := request.PathParameter("name")
 	mCtx := server.GlobalContext()
-	deployment, err := mCtx.Apps.Apps().V1().Deployment().Cache().Get(ns, name)
+	deployment, err := mCtx.Apps.Apps().V1().Deployment().Get(ns, name, v1.GetOptions{})
 	if err != nil {
+		logrus.Errorf("%s-%s:%s", ns, name, err.Error())
 		response.WriteError(500, err)
 		return
 	}
@@ -24,6 +27,7 @@ func Inject(request *restful.Request, response *restful.Response) {
 	update, err := mCtx.Apps.Apps().V1().Deployment().Update(deployment)
 
 	if err != nil {
+		logrus.Errorf("%s-%s:%s", ns, name, err.Error())
 		response.WriteError(500, err)
 		return
 	}

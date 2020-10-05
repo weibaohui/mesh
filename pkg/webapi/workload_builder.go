0836e60f7dfb120f8c6a4512cb9cd592d073728b
weibaohui <weibaohui@yeah.net>
2020-10-05 21:51:02 +0800

    workload load from name

--- pkg/webapi/workload_builder.go
+++ pkg/webapi/workload_builder.go
@@ -1,21 +1,44 @@
 package webapi
 
 import (
-	"k8s.io/kubernetes/pkg/apis/apps"
+	"fmt"
+
+	v1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/weibaohui/mesh/pkg/server"
 )
 
 type WorkLoadBuilder struct {
-	apps.Deployment
+	d *v1.Deployment
 }
 
+func (w *WorkLoadBuilder) Load(ns, name string) *WorkLoadBuilder {
+	mctx := server.GlobalContext()
+	deployment, err := mctx.K8s.AppsV1().Deployments(ns).Get(name, metav1.GetOptions{})
+	if err != nil {
+		return nil
+	}
+	w.d = deployment
+	return w
+}
 func (w *WorkLoadBuilder) Name(name string) *WorkLoadBuilder {
-	w.ObjectMeta.Name = name
+	w.d.ObjectMeta.Name = name
 	return w
 }
 
 func (w *WorkLoadBuilder) Labels(labels map[string]string) *WorkLoadBuilder {
-	w.ObjectMeta.Labels = labels
-	w.Spec.Template.Labels = labels
-	w.Spec.Selector.MatchLabels = labels
+	w.d.ObjectMeta.Labels = labels
+	w.d.Spec.Template.Labels = labels
+	w.d.Spec.Selector.MatchLabels = labels
+	return w
+}
+func (w *WorkLoadBuilder) AddLabels(k, v string) *WorkLoadBuilder {
+	labels := w.d.ObjectMeta.Labels
+	for k, v := range labels {
+		fmt.Println(k, v)
+	}
+	w.d.Spec.Template.Labels = labels
+	w.d.Spec.Selector.MatchLabels = labels
 	return w
 }
